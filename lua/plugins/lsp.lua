6980982ce24f224fd03a04a1cf57eb20d869e671
Ferenczi Janos <janoferenczi@yahoo.com>
2025-05-08 11:48:58 +0300

    Added lsp clients to the lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,22 @@
 return {
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'hrsh7th/cmp-path'},
+	{ 'neovim/nvim-lspconfig' },
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/nvim-cmp' },
+	{ 'hrsh7th/cmp-path' },
 	{
 		'williamboman/mason.nvim',
-		config = function ()
+		config = function()
 			require('mason').setup({})
 		end
 
 	},
 	{
 		'williamboman/mason-lspconfig.nvim',
-		config = function ()
+		config = function()
 			require('mason-lspconfig').setup({
 				-- Replace the language servers listed here
 				-- with the ones you want to install
-				ensure_installed = {'angularls', 'cssls'},
+				ensure_installed = { 'angularls', 'cssls', 'html', 'lua_ls', 'rust_analyzer', 'gopls', 'sqlls', 'ts_ls', 'zls' },
 				handlers = {
 					function(server_name)
 						require('lspconfig')[server_name].setup({})
